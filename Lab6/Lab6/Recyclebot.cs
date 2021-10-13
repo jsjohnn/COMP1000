7002e31efb9d26b0e12fb68ae6547dd903c0331b
john kim <jsjohan92@gmail.com>
2021-10-13 12:33:53 +0900

    no message

--- Lab6/Lab6/Recyclebot.cs
+++ Lab6/Lab6/Recyclebot.cs
@@ -41,12 +41,28 @@ namespace Lab6
             else
             {
                 List<Item> dump = new List<Item>(NonRecycleItems.Count);
+
+                //p:아이템의 부피가 10L, 11L 또는 15L가 아니다.
+                //  (10 or 11 or 15)' == 10' and 11' and 15'
+                //q:이는 그 아이템이 유독 폐기물임을 함의한다.
+                //r:이는 다시 그 아이템이 가구나 전기제품임을 함의한다.
+                //  (furniture or electronics)
+                // (p && !q) && (q && !r)
+
+                // TTT, FTT, FF
+
                 foreach (var i in NonRecycleItems)
                 {
-                    if (!(i.Volume != 10 && i.Volume != 11 && i.Volume != 15 && i.IsToxicWaste)
-                        && !(i.IsToxicWaste && i.Type != EType.Electronics && i.Type != EType.Furniture))
+                    if (i.Volume != 10 && i.Volume != 11 && i.Volume != 15 && i.IsToxicWaste == false)
                     {
                         dump.Add(i);
+                    }
+                    else
+                    {
+                        if (i.Type == EType.Furniture || i.Type == EType.Electronics)
+                        {
+                            dump.Add(i);
+                        }
                     }
                 }
 
