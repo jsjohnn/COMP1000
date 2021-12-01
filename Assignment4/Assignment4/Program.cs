cd5e7c7fe3a3c1d84e10301137abd4a0c34007ca
john kim <jsjohan92@gmail.com>
2021-12-01 12:57:58 +0900

    no message

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -10,16 +10,20 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            using (FileStream fs = File.OpenRead("/Users/donghwa/Desktop/COMP1000/Assignment4/Assignment4/earth.png"))
-            using (Bitmap image = new Bitmap(fs))
-            using (Bitmap newImage = SignalProcessor.ConvolveImage(image, new double[,] {
-                    { 1 / 9.0, 1 / 9.0, 1 / 9.0 },
-                    { 1 / 9.0, 1 / 9.0, 1 / 9.0 },
-                    { 1 / 9.0, 1 / 9.0, 1 / 9.0 }
-                   }))
-            {
-                newImage.Save("image_box_filtered.png", ImageFormat.Png); // 결과를 image_box_filtered.png 파일에 저장
-            }
+            Bitmap bitmap = new Bitmap(File.OpenRead("earth.png"));
+
+
+
+            //using (FileStream fs = File.OpenRead("/Users/donghwa/Desktop/COMP1000/Assignment4/Assignment4/earth.png"))
+            //using (Bitmap image = new Bitmap(fs))
+            //using (Bitmap newImage = SignalProcessor.ConvolveImage(image, new double[,] {
+            //        { 1 / 9.0, 1 / 9.0, 1 / 9.0 },
+            //        { 1 / 9.0, 1 / 9.0, 1 / 9.0 },
+            //        { 1 / 9.0, 1 / 9.0, 1 / 9.0 }
+            //       }))
+            //{
+            //    newImage.Save("image_box_filtered.png", ImageFormat.Png); // 결과를 image_box_filtered.png 파일에 저장
+            //}
         }
     }
 }
